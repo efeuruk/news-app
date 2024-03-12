31dd604060d892414caac47b7a82b7071556dc30
Ersoy Efe Uruk <efeuruk1@gmail.com>
2024-03-12 20:40:39 +0300

    apis and axios are added and tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
+import * as api from "./api/index";
 
 function App() {
+  useEffect(async () => {
+    // const res = await api.newsApi.get(
+    //   "/top-headlines?country=us&category=business"
+    // );
+    // const res = await api.guardianApi.get("/search?q=debate");
+    // const res = await api.nytimesApi.get("");
+    // console.log(res);
+  }, []);
   return <div>a</div>;
 }
 
---
+++ src/api/index.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const env = import.meta.env;
+
+// Create instances for each API source
+const newsApi = axios.create({
+  baseURL: env.VITE_NEW_API_BASE_URL,
+  headers: {
+    Authorization: env.VITE_NEWS_API_KEY,
+  },
+});
+
+const guardianApi = axios.create({
+  baseURL: env.VITE_GUARDIAN_API_BASE_URL,
+  headers: {},
+  params: {
+    "api-key": env.VITE_GUARDIAN_API_KEY,
+  },
+});
+
+const nytimesApi = axios.create({
+  baseURL: env.VITE_NYTIMES_API_BASE_URL,
+  headers: {},
+  params: {
+    "api-key": env.VITE_NYTIMES_API_KEY,
+  },
+});
+
+export { newsApi, guardianApi, nytimesApi };
